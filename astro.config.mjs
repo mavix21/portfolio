5cf1bee0f99fced12013d6af0f391701fcd7c337
mavix21 <marcevizcarra2109@hotmail.com>
2025-06-21 23:11:58 -0500

    chore: update site URL in astro.config.mjs to reflect new deployment address

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://marcelovizcarra.com",
+  site: "https://marcelo-vizcarra.netlify.app",
 
   vite: {
     plugins: [tailwindcss()],
